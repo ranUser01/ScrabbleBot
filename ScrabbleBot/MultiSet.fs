059c91e55c146f89d9bce4819d2f07177be50c3d
Maria Momanu <mmom@itu.dk>
2024-05-01 21:28:02 +0200

    added solutions from assignments to Eval and MultiSet

--- ScrabbleBot/MultiSet.fs
+++ ScrabbleBot/MultiSet.fs
@@ -2,8 +2,20 @@
 
 module internal MultiSet
 
-    type MultiSet<'a> = Temp of unit // Not implemented
+    type MultiSet<'a when 'a: comparison> = R of Map<'a, uint32>
 
-    let empty : MultiSet<'a> = Temp () // Not implemented
-    let add   : 'a -> uint32 -> MultiSet<'a> -> MultiSet<'a> = fun _ _ _ -> failwith "Not implemented" 
-    let fold  : ('b -> 'a -> uint32 -> 'b) -> 'b -> MultiSet<'a> -> 'b = fun _ _ _ -> failwith "Not implemented"
+    let empty = R Map.empty
+    
+    let numItems (a: 'a) (R s) : uint32 =
+        match Map.tryFind a s with
+        | Some count -> count
+        | None -> 0u
+        
+    let add (a: 'a) (n: uint32) (R s) : MultiSet<'a> =
+        if n = 0u then
+            R s
+        else
+            let currentNumItems = numItems a (R s)
+            R (Map.add a (currentNumItems + n) s)
+    let fold (f: 'b -> 'a -> uint32 -> 'b) (acc: 'b) (R s) : 'b =
+        Map.fold (fun acc k v -> f acc k v) acc s
